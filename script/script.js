519ee1a462e6bb72e18de58a186b908c3e3074b4
Bazif Abdullah <bazifabdullah@gmail.com>
2024-04-01 18:54:15 +0500

    Add Script

---
+++ script/script.js
@@ -0,0 +1,6 @@
+const inputFieldData = document.querySelectorAll('.data')
+const formControl = document.getElementById('signup-form');
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
